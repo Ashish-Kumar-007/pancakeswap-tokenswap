17ef2c8815493267bbf68d7ae32e27c7a7902e44
Ash45# <ashishsahoo2015@gmail.com>
2023-05-26 19:59:56 +0530

    Ashish: Code modified

--- connect.js
+++ connect.js
@@ -21,6 +21,7 @@ const connectToMetaMask = async () => {
     Status.textContent = "Error connecting to MetaMask";
   }
 };
+
 const routerAddress = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
 const ABI = [
   {
@@ -455,6 +456,345 @@ const ABI = [
   { stateMutability: "payable", type: "receive" },
 ];
 
+const tokenABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "delegator",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "fromDelegate",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "toDelegate",
+        type: "address",
+      },
+    ],
+    name: "DelegateChanged",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "delegate",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "previousBalance",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "newBalance",
+        type: "uint256",
+      },
+    ],
+    name: "DelegateVotesChanged",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "DELEGATION_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "DOMAIN_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "uint32", name: "", type: "uint32" },
+    ],
+    name: "checkpoints",
+    outputs: [
+      { internalType: "uint32", name: "fromBlock", type: "uint32" },
+      { internalType: "uint256", name: "votes", type: "uint256" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "subtractedValue", type: "uint256" },
+    ],
+    name: "decreaseAllowance",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "delegatee", type: "address" }],
+    name: "delegate",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "delegatee", type: "address" },
+      { internalType: "uint256", name: "nonce", type: "uint256" },
+      { internalType: "uint256", name: "expiry", type: "uint256" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "delegateBySig",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "delegator", type: "address" }],
+    name: "delegates",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "getCurrentVotes",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "account", type: "address" },
+      { internalType: "uint256", name: "blockNumber", type: "uint256" },
+    ],
+    name: "getPriorVotes",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "addedValue", type: "uint256" },
+    ],
+    name: "increaseAllowance",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_to", type: "address" },
+      { internalType: "uint256", name: "_amount", type: "uint256" },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
+    name: "mint",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "nonces",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "numCheckpoints",
+    outputs: [{ internalType: "uint32", name: "", type: "uint32" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "recipient", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "sender", type: "address" },
+      { internalType: "address", name: "recipient", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
 const interactWithContract = async () => {
   try {
     if (typeof window.ethereum === "undefined") {
@@ -466,54 +806,61 @@ const interactWithContract = async () => {
     const web3Provider = new Web3(window.ethereum);
     const accounts = await web3Provider.eth.getAccounts();
     const tokenIn = "0xFa60D973F7642B748046464e165A65B7323b0DEE";
+    const tokenInContract = new web3Provider.eth.Contract(tokenABI, tokenIn);
     const tokenOut = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd";
-    const amountIn = web3Provider.utils.toWei("1", "ether");
     const pancakeswap = new web3Provider.eth.Contract(ABI, routerAddress);
 
     try {
+      // Get the token balance of the user's wallet
+      const tokenBalance = await tokenInContract.methods.balanceOf(accounts[0]).call();
+      
+      // Convert 1 CAKE to its corresponding token amount
+      const amountIn = web3Provider.utils.toWei("1", "ether");
+      
+      // Ensure that the token balance is sufficient for the input amount
+      if (web3Provider.utils.toBN(tokenBalance).lt(web3Provider.utils.toBN(amountIn))) {
+        throw new Error("Insufficient token balance in the user's wallet.");
+      }
+
       const amounts = await pancakeswap.methods
         .getAmountsOut(amountIn, [tokenIn, tokenOut])
         .call();
-      console.log(amounts.toString());
+
+      const slippageTolerance = 0.01; // 1% slippage tolerance
+      const amountOutMin = web3Provider.utils.toBN(amounts[1]).muln(1 - slippageTolerance);
       const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
-      console.log(deadline);
-
-      const amountOutMin = new web3Provider.utils.BN(amounts[1].toString());
-
-      console.log(`
-        Buying new token
-        tokenIn: ${web3Provider.utils.fromWei(
-          amountIn,
-          "ether"
-        )} ${tokenIn} (CAKE)
-        tokenOut: ${amountOutMin}
-        )} ${tokenOut} (USDT)
-     `);
+      
+      // Approve the router to spend the input amount of tokens
+      await tokenInContract.methods.approve(routerAddress, amountIn).send({
+        from: accounts[0],
+      });
 
       const tx = await pancakeswap.methods
-        .swapExactTokensForTokens(
+        .swapTokensForExactTokens(
           amountIn,
-          amounts[1],
+          amountOutMin,
           [tokenIn, tokenOut],
           accounts[0],
           deadline
         )
-        .send({ from: accounts[0] });
-      console.log("tx status : ", tx);
-    } catch (error) {
-      console.error("Error executing the transaction:", error);
-    }
-    // Call a function on the contract
-    // const result = await contract.methods.someFunction().call();
-    // console.log(result);
+        .send({ from: accounts[0], gas: 500000 }); // Adjust the gas limit as needed
 
-    // Display the result on the webpage
-    // result.textContent = `Result: ${result}`;
+      console.log("tx status: ", tx);
+
+      // Handle success and provide user feedback
+      console.log("Transaction successful!");
+      // Display success message or perform additional actions
+    } catch (error) {
+      console.error("Error interacting with the contract:", error);
+      // Display error message or handle error appropriately
+    }
   } catch (error) {
     console.error("Error interacting with the contract:", error);
-    // result.textContent = "Error interacting with the contract";
   }
 };
+
+
+
 
 connectButton.addEventListener("click", connectToMetaMask);
 getDataButton.addEventListener("click", interactWithContract);
